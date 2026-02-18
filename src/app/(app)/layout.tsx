0e72050e36b62b39437f28dfd538499dfef2f8f9
passportdog <therealpassportdog@gmail.com>
2026-02-18 06:25:25 -0500

    Delete src directory

--- src/app/(app)/layout.tsx
+++
@@ -1,7 +0,0 @@
-export const dynamic = 'force-dynamic'
-
-import AppShellLayout from './app-shell'
-
-export default function AppLayout({ children }: { children: React.ReactNode }) {
-  return <AppShellLayout>{children}</AppShellLayout>
-}
